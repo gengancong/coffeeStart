7305c0529e547d50d67a1b7264c1f0afe3ffd3ad
gengancong <gengancong@qq.com>
2015-07-22 09:17:41 +0800

    格式化

---
+++ src/main/java/com/coffee/web/sys/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.coffee.web.sys.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+	@RequestMapping("index.do")
+	public String index() {
+		logger.debug("ok.");
+		return "index";
+	}
+}
